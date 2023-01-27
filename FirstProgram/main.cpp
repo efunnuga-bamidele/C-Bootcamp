a5bb4e783f6dfd98bacc011d5bbda15c38f5e547
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-01-27 14:46:10 +0100

    First Project

---
+++ FirstProgram/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int favouriteNumber;
+	std::cout << "Enter youu favourite number between 1 and 100: ";
+	std::cin >> favouriteNumber;
+	std::cout << "Amazing!! That's my favourite number too!" << std::endl;
+	return 0;
+}
